0742669bf84d58254b861393dfe3de3d1a58c2be
markcascardi <mcascardi@gmail.com>
2020-07-28 12:16:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+
+let random = Math.random()
+
+
+let num3 = 34
+let num4 = 6
+
+let mod = num3 % num4
+
+let num5 = (1, 2, 3, 20) 
+
+let max = Math.floor(num5)
